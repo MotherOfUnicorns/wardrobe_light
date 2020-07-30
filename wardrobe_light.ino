9ebdc68d4c94c7991d3e18bfd0c004d4df80b280
yun <elfie_9999@yahoo.com>
2020-07-30 10:19:54 +0200

    allow multiple lights for each wardrobe door

--- wardrobe_light.ino
+++ wardrobe_light.ino
@@ -1,36 +1,54 @@
-const int switchPin = 6;
-const int lightPin = 4;
-const bool switchNormallyOn = false;
+const int switchPin[2] = {6, A0};
+const int lightPin[2] = {4, 8};
+const bool switchNormallyOn[2] = {false, false};
 const long second = 1000;
 const long lightDelay = 10; // in seconds
 
-long timeCounter = 0; // in seconds
-int switchState = 0;
+long timeCounter[2] = {0, 0}; // in seconds
+int switchState[2] = {0, 0};
 
 void setup() {
-    pinMode(switchPin, INPUT);
-    pinMode(lightPin, OUTPUT);
+    for (int i = 0; i < 2; i++) {
+        pinMode(switchPin[i], INPUT);
+        pinMode(lightPin[i], OUTPUT);
+    }
     Serial.begin(9600);
 }
 
-void loop() {
-    switchState = digitalRead(switchPin);  
-    Serial.println(timeCounter);
 
-    if (switchState == switchNormallyOn){
-        digitalWrite(lightPin, LOW);
-        Serial.println("light off");
-        delay(second);
-        timeCounter = 0;
-    } else if (timeCounter < lightDelay) {
-        digitalWrite(lightPin, HIGH);
-        Serial.println("light on");
-        delay(second);
-        timeCounter++;
+void updateLightState(int n) {
+    if (switchState[n] == switchNormallyOn[n]) {
+        digitalWrite(lightPin[n], LOW);
+        Serial.print("Light #");
+        Serial.print(n);
+        Serial.println(": off");
+        timeCounter[n] = 0;
+    } else if (timeCounter[n] < lightDelay) {
+        digitalWrite(lightPin[n], HIGH);
+        Serial.print("Light #");
+        Serial.print(n);
+        Serial.println(": on");
+        timeCounter[n]++;
     } else {
-        digitalWrite(lightPin, LOW);
-        Serial.println("light off");
-        delay(second);
+        digitalWrite(lightPin[n], LOW);
+        Serial.print("Light #");
+        Serial.print(n);
+        Serial.println(": off");
+    }
+}
+
+
+void loop() {
+    for (int i = 0; i < 2; i++) {
+        switchState[i] = digitalRead(switchPin[i]);
+        Serial.print("Light #");
+        Serial.print(i);
+        Serial.print(" time counter: ");
+        Serial.println(timeCounter[i]);
     }
 
+    for (int i = 0; i < 2; i++) {
+        updateLightState(i);
+    }
+    delay(second);
 }
